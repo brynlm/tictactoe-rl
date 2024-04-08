473d1dc17617a31d265d11794548fa0b237f7300
brynlm <brynleemeyer@gmail.com>
2024-04-08 16:57:52 -0700

    utils updated to allow RLNN to play Xs or Os

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ from tictactoegame import TicTacToe as Board
 # In[5]:
 
 
-def game_step(policy, start=False):
+def game_step(p1=None, p2=None, start=False, record='p1', conv=False):
     board = Board()
     if isinstance(start, type(np.full((3,3),0))):
         board.board=np.copy(start)
@@ -23,27 +23,63 @@ def game_step(policy, start=False):
         if board.isGameOver(1) or board.isGameOver(-1):
             yield np.copy(board.board)
             break
-        S_t = board_to_features(np.copy(board.board))
-#         A_t = get_action(tf.reshape(policy(S_t), (9,1)).numpy())
-        A_t = get_action(tf.squeeze(policy(tf.reshape(S_t, (1,18)))).numpy())
-        yield (A_t, np.copy(board.board))
-        if board.board[A_t[0], A_t[1]] != 0:
-            board.board[A_t[0], A_t[1]] = 9
-            continue
-        board.setPiece(A_t, 1)
+            
+        if p1 is None:
+            actions = board.getPosns(0)
+            if len(actions) == 0:
+                yield np.copy(board.board)
+                break
+            i = np.random.default_rng().integers(len(actions))
+            board.setPiece(actions[i], 1)
+        else:
+            if conv==False:
+                S_t = board_to_features(np.copy(board.board))
+                A_t = get_action(tf.squeeze(p1(tf.reshape(S_t, (1,18)))).numpy())
+            else:
+                S_t = np.expand_dims(np.copy(board.board), axis=0)
+                A_t = get_action(tf.squeeze(p1(S_t,conv=True)).numpy())
+            if record=='p1':
+                yield (A_t, np.copy(board.board))
+        
+            if board.board[A_t[0], A_t[1]] != 0: # <-- might not need this anymore? 
+                board.board[A_t[0], A_t[1]] = 9
+                continue
+            board.setPiece(A_t, 1)
+            
+        if board.isGameOver(1) or board.isGameOver(-1):
+            yield np.copy(board.board)
+            break
+            
         actions = board.getPosns(0)
         if len(actions) == 0:
             yield np.copy(board.board)
             break
-        i = np.random.default_rng().integers(len(actions)) #<--- rework this part to allow games between models
-        board.setPiece(actions[i], -1)
+            
+        if p2 is None:
+            i = np.random.default_rng().integers(len(actions))
+            board.setPiece(actions[i], -1)
+            continue 
+            
+        if conv==False:
+            S_t = board_to_features(np.copy(board.board)*-1)
+            A_t = get_action(tf.squeeze(p2(tf.reshape(S_t, (1,18)))).numpy())
+        else:
+            S_t = np.expand_dims(np.copy(board.board)*-1, axis=0)
+            A_t = get_action(tf.squeeze(p2(S_t,conv=True)).numpy())
+
+        if record=='p2':
+            yield (A_t, np.copy(board.board))
+            
+        if board.board[A_t[0], A_t[1]] != 0: # <-- might not need this anymore? 
+            board.board[A_t[0], A_t[1]] = 9
+            continue
+        board.setPiece(A_t, -1)
     return
      
-def gen_episode(policy, state=np.full((3,3), 0)):
+def gen_episode(p1=None,p2=None, state=np.full((3,3), 0), record='p1',conv=False):
     episode = []
-    for i in game_step(policy, state):
+    for i in game_step(p1, p2, state, record,conv):
         episode.append(i)
-        
     return episode
 
 def board_to_features(board):
@@ -51,7 +87,7 @@ def board_to_features(board):
     p1_posn = np.asarray((board==1))
     p2_posn = np.asarray((board==-1))
     features = np.concatenate([p1_posn, p2_posn], axis=0)
-    return features
+    return features.astype(int)
 
 
 # Could it make a difference by representing features as 9x2 array instead of 18x1?
@@ -64,6 +100,8 @@ def one_hot(board):
     
 
 def get_action(probs, batch_size=None):
+#     print(probs)
+    probs += 0.000001
     probs /= probs.sum()
     p = np.copy(probs)
 #     print(p)
@@ -144,7 +182,7 @@ def expected_return(state, policy,reward_fn, gamma, return_fn=False, num_trials=
 #         reward += reward_fn(ep)
 #     return reward/num_trials
 
-def state_dist(policy, num_trials, state=np.full((3,3), 0)):
+def state_dist(p1,num_trials,p2=None,record='p1', state=np.full((3,3), 0)):
     illegal_moves = 0
     lost = 0
     won = 0
